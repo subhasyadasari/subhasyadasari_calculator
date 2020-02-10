2c4d7b645b860fb6fef72ced5fe7575a4f035198
subhasyadasari <60658109+subhasyadasari@users.noreply.github.com>
2020-02-10 22:04:36 +0530

    Add files via upload

---
+++ Calculators.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Calculators extends Operation
+{
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter two numbers:");
+	int num1=sc.nextInt();
+	int num2=sc.nextInt();
+    System.out.println("Enter your options:");
+    System.out.println("\n1. Division");
+    System.out.println("2. Multiplication");
+    System.out.println("3. Addition");
+    System.out.println("4. Subtraction");
+    
+    int ch=sc.nextInt();
+    if(ch==1)
+    {
+    	DIVISION(num1,num2);
+    }    
+    else if(ch==2)
+    {
+    	MULTIPLICATION(num1,num2);
+    }
+    else if(ch==3)
+    {
+    	ADDITION(num1,num2);
+    }	
+    else if(ch==4)
+    {
+    	SUBRACTION(num1,num2);
+    }
+    else
+    {
+    	System.out.println("Invalid choice");
+    }
+
+}
+}
---
+++ Operation.java
@@ -0,0 +1,28 @@
+
+public class Operation {
+        public static  void DIVISION(int A,int B)
+	{
+		int c=A/B;
+		System.out.println(c);
+	}
+        public static void MULTIPLICATION(float A,float B)
+	{
+		int c=A*B;
+		System.out.println(c);
+	}
+	public static void ADDITION(int A,int B)
+	  {
+		 int c=A+B;
+		  System.out.println(c);
+	  }
+      
+	public static void SUBRACTION(int A,int B)
+	{
+		int c=A-B;
+	    System.out.println(c);
+	}
+	
+	
+
+
+}
